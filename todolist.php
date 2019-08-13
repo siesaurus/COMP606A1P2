24cf0c97237be00f0c1f8a7db76841e2dbb6dabe
siesaurus <52937221+siesaurus@users.noreply.github.com>
2019-08-13 14:04:08 +1200

    Tweaked style.css

--- todolist.php
+++ todolist.php
@@ -11,7 +11,7 @@
 <script src ="script.js"></script>
 <div id="myDIV" class="header">
   <h2 style="margin:5px">A JQuery To-Do List</h2>
-  <input type="text" id="myInput" placeholder="Title...">
+  <input type="text" id="myInput" placeholder="Add something new to dooo....">
   <span onclick="newElement()" class="addBtn">Add</span>
 </div>
 
